a326db3d84f3d8e19de39ccbee6b5efbd9d9d2c9
kk-blue <474604082@qq.com>
2019-04-01 20:27:30 +0800

    [LuZhi]简单Socket消息通信添加注释

--- src/client/SimpleSocketClient.java
+++ src/client/SimpleSocketClient.java
@@ -6,9 +6,12 @@ import java.net.Socket;
 
 public class SimpleSocketClient {
     public static void main(String[] args) throws IOException {
+//        连接的服务IP地址和端口号
         String host="127.0.0.1";
         int port=1227;
+//        与服务端建立连接
         Socket socket=new Socket(host,port);
+//        建立连接后获得输出流
         OutputStream outputStream=socket.getOutputStream();
         String msg="你好！！Hello World!";
         socket.getOutputStream().write(msg.getBytes("UTF-8"));
--- src/server/SimpleSocketServer.java
+++ src/server/SimpleSocketServer.java
@@ -7,15 +7,19 @@ import java.net.Socket;
 
 public class SimpleSocketServer {
     public static void main(String[] args) throws IOException {
+//        监听指定端口
         int port=1227;
         ServerSocket serverSocket=new ServerSocket(port);
+//        server等待连接的到来
         System.out.println("waiting......");
         Socket socket=serverSocket.accept();
+//        建立好连接，从socket中获取输入流，建立缓冲区进行读取
         InputStream inputStream=socket.getInputStream();
         byte[] bytes=new byte[1024];
         int len;
         StringBuilder sb=new StringBuilder();
         while((len=inputStream.read(bytes))!=-1){
+//            注意指定编码格式，发送，接受格式要统一
             sb.append(new String(bytes,0,len,"UTF-8"));
         }
         System.out.println("Get message from Client:"+sb);
